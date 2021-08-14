e9bc33b151eec6c6cc7b07875aee7449c06b695f
parksoyoung <soyoung931014@gmail.com>
2021-08-14 15:20:44 +0900

    react 연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Food(props){
+
+
+  return <h1> I like {props.fav}</h1>;
 }
 
-export default App;
+function App() {
+   /*component는 보다시피  html을 반환하는 함수야*/  
+  return  (<div>   
+            <h1>hello</h1>
+            <Food 
+            fav = "kimchi"
+            something = {true}
+            papapa = {["hello",123,true]} />
+           <Food fav= "lamen" />
+           <Food fav= "coffee" />
+           <Food fav= "potato" />
+           </div>
+  );
+        
+}
+export default App ;
+ 
+
